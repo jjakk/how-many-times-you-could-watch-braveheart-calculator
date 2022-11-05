09972262ada464de3f30710f93b944cdccceead6
John Khachian <jaksawesome10@gmail.com>
2022-11-05 13:04:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ const formValidation = () => {
     ];
     
     elements.forEach(e => {
-        if(e.value === ""){
-            e.value = 0;
-        }
-        else if(e.value > e.max){
-            e.value = e.max
-        }
-        else if(e.value < e.min){
-            e.value = e.min
-        }
+        if(e.value === "") e.value = 0;
+        else if(e.value > e.max) e.value = e.max
+        else if(e.value < e.min) e.value = e.min;
     });
 };
 
